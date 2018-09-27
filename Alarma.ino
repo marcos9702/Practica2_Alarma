4a642f4cc5c6dc79294ba5e85b76c353ea7c2588
Marcos Flores <marcos9702@gmail.com>
2018-09-26 23:00:57 -0500

    version 2

--- Alarma.ino
+++ Alarma.ino
@@ -1,21 +1,60 @@
-const int pinSensor = 2;
-const int pinSalida = 12;
-volatile boolean enciende;
+/* Flores Bonilla José Marcos
+ * Instituto Tecnologico de León
+ * Ingeniería en Sistemas Computacionales
+ * Sistemas Programables 
+ * 
+ * Alarma notificadora de seguridad
+ */
+
+//delcaramos de constantes 
+const int intervaloTiempo = 250; // intervalo de tiempo para controlar ruido
+const int pinSensor = 2;         // pin del Reed Switch
+const int pinSalida = 12;        // pin de salida para led y buzzer
+const int pinBoton = 3;          // pin para pushBoton
+
+//declaracion de variables
+volatile int ISRContador = 0;    // contabilizar el tiempo de ejecucion de Reed Switch
+volatile boolean enciende;       // encender o apagar la Reed Switch
+int contadorReal = 0;            // mostrar en monitor el tiempo de ejecucion acumulable
+long contaTiempo1 = 0;           // tiempo de ejecucion de Reed Switch
+long contaTiempo2 = 0;           // tiempo de ejecucion de pushBoton
 
 void setup(){
-  pinMode(pinSnesor, INPUT_PULLUP);
+  pinMode(pinSensor, INPUT_PULLUP);
+  pinMode(pinBoton , INPUT_PULLUP);
   pinMode(pinSalida,OUTPUT);
-  enciende=false;
-  attachInterrupt(0, interrupcion,CHANGE);
+  Serial.begin(9600);
+  enciende=false; 
+  //interrupcion de Reed Switch cuando esta apagado a encendido
+  attachInterrupt(0, interrupcionEncendido,RISING);
+  //interrupcion de PushBoton cuando es presioando
+  attachInterrupt(1, interrupcionAPagado,FALLING);
 }
 
 void loop(){
   if (enciende==true){
+    contadorReal = ISRContador; //guardamos tiempo de ejecucion de reed y mostramos
+    Serial.println(contadorReal);
     digitalWrite(pinSalida,HIGH);
-  }else{
-    digitalWrite(pinSalida,LOW);}
+  }else
+    digitalWrite(pinSalida,LOW);
 }
 
-void interrupcion(){ 
-  enciende=!enciende; 
+//interrupcion que funciona cuando el switch cambia de estado
+void interrupcionEncendido(){ 
+  //verificamos si no es un efecto rebote y cambiamos de estado "enciende"
+  if (millis() > contaTiempo1 + intervaloTiempo){
+    ISRContador++;
+    contaTiempo1 = millis();
+    enciende=true; 
+  }
+}
+
+//interrupcion que funciona cuando el pushBoton es presionado
+void interrupcionAPagado(){ 
+    //verificamos si no es un efecto rebote de pushBoton y cambiamos de estado "enciende"
+  if (millis() > contaTiempo2 + intervaloTiempo){
+    contaTiempo2 = millis();
+    enciende=false; 
+  }
 }
---
+++ readme.md
@@ -0,0 +1,41 @@
+# Práctica2: Alarma_Seguridad
+Sistema de alarma de seguridad 
+
+  >Creado por:  Flores Bonilla José Marcos; 
+  >Contacto:    marcos9702@gmail.com
+
+#### OBJETIVO:
+  -Desarrollar una alarma para cuando algun individuo entre sin autorizacion esta se active y solo podra ser desactivada por el encargado
+
+  - Reducir el consumo de energía al mínimo. Mientras la alarma no esté sonando el arduino deberá consumir la mínima cantidad de         energía. Cuando la puerta se cierre esta deberá de dejar de sonar y regresar al estado de bajo consumo energético. Para ello se utilizaran
+interrupciones.
+
+
+
+#### HERRAMIENTAS DE DESARROLLO:
+  - Arduino IDE
+  - Fritzing
+
+
+#### MATERIALES:
+  - Arduino UNO
+  - Protoboard
+  - Reed switch
+  - Buzzer
+  - Led
+  - Resistencias (1K, 220 ohms)
+  - Imán
+  - Cables
+  - Jumpers
+
+
+# Diagrama circuito
+
+![alt tag](https://github.com/marcos9702/Practica2_Alarma/blob/master/diagrama.png)
+
+# Diagrama en protoboard
+
+![alt tag](https://github.com/marcos9702/Practica2_Alarma/blob/master/dise%C3%B1o.png)
+
+# Resultados
+
